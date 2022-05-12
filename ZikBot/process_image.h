e217f16296f619b3339638da2850ed82b3e01fbd
Julie <julie.bohning@epfl.ch>
2022-05-12 09:08:55 +0200

    commentaire

--- ZikBot/process_image.h
+++ ZikBot/process_image.h
@@ -2,6 +2,7 @@
 #define PROCESS_IMAGE_H
 
 #define MAX_LINE_NBR 3 // MAX_LINE_NBR - 2 (margins) = nbr of notes permitted at the same time music arrangement (here no arrangement permitted)
+//TO DO: ajouter MAX_DETECTABLE_LINE_NBR 10 pour pouvoir avoir des nbr=5 p.ex qui font une erreur.
 
 extern uint8_t note_rel_pos; //[%]
 
